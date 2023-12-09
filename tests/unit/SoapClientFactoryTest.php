df0da7361c1ed676f324c49a73153dab30c0caa3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-09 17:53:01 +0400

    Setup test.

--- tests/unit/SoapClientFactoryTest.php
+++ tests/unit/SoapClientFactoryTest.php
@@ -3,10 +3,19 @@
 namespace rocketfellows\SoapClientFactory\tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\SoapClientFactory\SoapClientFactory;
 
 /**
  * @group soap-client-factory
  */
 class SoapClientFactoryTest extends TestCase
 {
+    private $soapClientFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->soapClientFactory = new SoapClientFactory();
+    }
 }
